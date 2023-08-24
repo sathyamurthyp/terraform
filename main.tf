b378423bee911e67b9159c518d244f76363e37a5
Sathyamurthy <sathyamurthyp@users.noreply.github.com>
2023-08-24 09:58:59 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region  = "us-west-2" # Change this to your desired region.
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
